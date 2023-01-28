fc93dec9c3c96fe547da3a0b72eabc348ae53e3b
Griffin <ffirgn@gmail.com>
2023-01-28 00:08:58 -0800

    color change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div name='about' className='w-full h-screen bg-[#39393A] text-[#FFFAFF]'>
 
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-[#]'>Hello, I'm</p>
+        <p className='text-[#81968F]'>Hello, I'm</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#FFFAFF]'>Griffin Hobbs</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#81968F]'>I'm a Full-Stack Developer.</h2>
         <p className='text-[#81968F] py-4 max-w-[700px]'>I'm a full-stack developer who is passionate in creating beautiful, user-friendly digital experiences.
@@ -14,7 +14,7 @@ const About = () => {
           I'm a highly motivated, fast learner who is always looking to improve my skills and learn new technologies.</p>
 
           <div>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#6153CC] hover:border-[#6153CC]'>My Projects 
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#7A8B99] hover:border-[#7A8B99]'>My Projects 
               <span className='group-hover:rotate-90 duration-300'>
                 <HiArrowNarrowRight className='ml-1'/>
               </span>
