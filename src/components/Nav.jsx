fb5bb35ae92e13f96eac8cc1eab406340f0e1ce5
Griffin <ffirgn@gmail.com>
2023-01-28 11:20:27 -0800

    updated nav to behave in one page feature with no scrolling with help from classmate

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,69 +1,26 @@
-import React, { useState } from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
-import { Link } from 'react-scroll'
+import React from 'react'
 
-const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggle = () => setIsOpen(!isOpen)
+function Nav(props) {
+  const {
+    pages = [],
+    setCurrentPage,
+    currentPage,
+  } = props;
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#39393A] text-[#FFFAFF]">
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#39393A] text-[#FFFAFF]'>
       <div>
         <h1 className="text-5xl text-[#81968F] font-bold">G</h1>
-      </div>
-
-      {/* Desktop Menu */}
-        <ul className='hidden md:flex'>
-          <li>
-          <Link to='about' smooth={true} duration={500} >
-          About
-        </Link>
-          </li>
-          <li>
-          <Link to='portfolio' smooth={true} duration={500} >
-          Portfolio
-        </Link>
-          </li>
-          <li>
-          <Link to='contact' smooth={true} duration={500} >
-          Contact
-        </Link>
-          </li>
-          <li>
-          <Link to='resume' smooth={true} duration={500} >
-          Resume
-        </Link>
-          </li>
-        </ul>
-
-      <div onClick={toggle} className='md:hidden z-10'>
-        {!isOpen ? <FaBars/> : <FaTimes/>}
-      </div>
-
-      {/* Mobile Menu */}
-        <ul className={!isOpen ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#39393A] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>
-            <Link onClick={toggle} to='about' smooth={true} duration={500} >
-          About
-        </Link>
+        </div>
+          <ul className="flex">
+            {pages.map((Page) => (
+            <li className={`mx-5 ${currentPage.name === Page.name && 'navActive'}`}key={Page.name}>
+              <span onClick={() => setCurrentPage(Page)}>
+                {Page.name}
+              </span>
             </li>
-            <li className='py-6 text-4xl'>
-            <Link onClick={toggle} to='portfolio' smooth={true} duration={500} >
-          Portfolio
-        </Link>
-            </li>
-            <li className='py-6 text-4xl'>
-            <Link onClick={toggle} to='contact' smooth={true} duration={500} >
-          Contact
-        </Link>
-            </li>
-            <li className='py-6 text-4xl'>
-            <Link onClick={toggle} to='resume' smooth={true} duration={500} >
-          Resume
-        </Link>
-            </li>
-          </ul> 
-
+            ))}
+          </ul>
     </div>
   )
 }
