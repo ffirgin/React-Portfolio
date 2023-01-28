97cd4e194a5358ffed0a740d4b09bf10234c8d7e
Griffin <ffirgn@gmail.com>
2023-01-28 00:08:21 -0800

    building out app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Nav from "./components/Nav";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <Nav />
+      <div id='trailer'>
+        <Nav />
+        <About />
+        <Portfolio />
+        <Contact />
+        <Resume />
+        <Footer />
+      </div>
     </div>
   );
 }
