9610d12a38849f2c5eea91da7d2e739261b05e3c
Griffin <ffirgn@gmail.com>
2023-01-28 11:29:13 -0800

    removed my project button after adding one page function

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
+import { AiOutlineSmile } from 'react-icons/ai'
 
 const About = () => {
   return (
@@ -12,14 +12,6 @@ const About = () => {
         <p className='text-[#81968F] py-4 max-w-[700px]'>I'm a full-stack developer who is passionate in creating beautiful, user-friendly digital experiences.
           I recently received my certificate in full-stack development from the University of California Irvine's Coding Bootcamp.
           I'm a highly motivated, fast learner who is always looking to improve my skills and learn new technologies.</p>
-
-          <div>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#7A8B99] hover:border-[#7A8B99]'>My Projects 
-              <span className='group-hover:rotate-90 duration-300'>
-                <HiArrowNarrowRight className='ml-1'/>
-              </span>
-            </button>
-          </div>
       </div>
 
     </div>
