8285bc3c4bc31a3fbf399e3f8df71e4a8f455b5a
Griffin <ffirgn@gmail.com>
2023-03-05 09:22:23 -0800

    changed text color for description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <p className='text-[#81968F]'>Hello, I'm</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#FFFAFF]'>Griffin Hobbs</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#81968F]'>I'm a Full-Stack Developer.</h2>
-        <p className='text-[#81968F] py-4 max-w-[700px]'>I'm a full-stack developer who is passionate in creating beautiful, user-friendly digital experiences.
+        <p className='text-[#FFFAFF] py-4 max-w-[700px]'>I'm a full-stack developer who is passionate in creating beautiful, user-friendly digital experiences.
           I recently received my certificate in full-stack development from the University of California Irvine's Coding Bootcamp.
           I'm a highly motivated, fast learner who is always looking to improve my skills and learn new technologies.</p>
       </div>
